036f0a390a6d7f4a47d4bc939e948efd10372b83
OriErez3 <orierez3@gmail.com>
2026-05-06 10:19:17 -0400

    Finished the vega calculations.

--- sabr.py
+++ sabr.py
@@ -84,7 +84,12 @@ def vega_BS(F: float, K:float, r: float, T: float, sigma:float) -> float:
         d = d1(F,K,sigma,T)
         vega_BS = F * np.exp(-r*T) * norm.pdf(d) * np.sqrt(T)
         return vega_BS
-
+def dsigma_dalpha(F: float, K: float, T: float, alpha: float, beta:float, rho:float, nu:float):
+    eps = 1e-4
+    sigma_up = sabr_vol(F,K,T,alpha+eps,beta,rho,nu)
+    sigma = sabr_vol(F,K,T,alpha,beta,rho,nu)
+    dsig_dalp = (sigma_up-sigma)/eps
+    return dsig_dalp
 
 
 class SABRModel:
@@ -104,7 +109,9 @@ class SABRModel:
     
     #def delta(self, F: float, K: float, T: float, alpha: float, beta:float, rho:float, nu:float, r:float) -> float:
 
-    #def vega(self, F: float, ):
+    def vega(self, F: float, K: float, T: float, r:float) -> float:
+        sigma = self.vol(F,K,T)
+        return vega_BS(F,K,r,T,sigma)*dsigma_dalpha(F,K,T,self.alpha,self.beta,self.rho,self.nu)
         
 
 
